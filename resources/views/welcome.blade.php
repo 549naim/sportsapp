ee740acec627555432784d6a162f8885e218801b
549naim <swenaim35549@gmail.com>
2024-06-24 12:56:53 +0600

    first

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>SPORTS STREAM| LIVE</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link href="css/style.css" rel="stylesheet" type="text/css" />
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<script src="js/jquery-1.4.2.min.js" type="text/javascript"></script>
+<script src="js/cufon-yui.js" type="text/javascript"></script>
+<script src="js/cufon-replace.js" type="text/javascript"></script>
+<script src="js/Gill_Sans_400.font.js" type="text/javascript"></script>
+<script src="js/script.js" type="text/javascript"></script>
+<!--[if lt IE 7]>
+<script type="text/javascript" src="js/ie_png.js"></script>
+<script type="text/javascript">ie_png.fix('.png, .link1 span, .link1');</script>
+<link href="css/ie6.css" rel="stylesheet" type="text/css" />
+<![endif]-->
+</head>
+<body id="page1">
+<!-- START PAGE SOURCE -->
+<div class="tail-top">
+  <div class="tail-bottom">
+    <div id="main">
+      <div id="header">
+        <div class="row-1">
+          <div class="fleft"><a href="#">Sportsapp stream <span>Live</span></a></div>
+          <ul>
+            <li><a href="#"><img src="images/icon1-act.gif" alt="" /></a></li>
+            <li><a href="#"><img src="images/icon2.gif" alt="" /></a></li>
+            <li><a href="#"><img src="images/icon3.gif" alt="" /></a></li>
+          </ul>
+        </div>
+        <div class="row-2">
+          <ul>
+            <li><a href="index.html" class="active">Home</a></li>
+            <li><a href="about-us.html">About</a></li>
+            <li><a href="articles.html">Articles</a></li>
+            <li><a href="contact-us.html">Contacts</a></li>
+            <li class="last"><a href="sitemap.html">Sitemap</a></li>
+          </ul>
+        </div>
+      </div>
+      <div id="content">
+        <div id="slogan">
+          
+          <div class="mx-5 px-5">
+            <h2 class="mx-5 px-5">Welcome to Sports Stream Live!<span>Your Premier Destination for Live Sports Streaming</span></h2>
+            <p >At SportsStream Live, we bring the excitement of live sports right to your screen. Whether you're a fan of football, basketball, soccer, tennis, or any other sport, we've got you covered with high-quality streams of your favorite games. Our platform is designed for sports enthusiasts who want to experience the thrill of live action, anywhere, anytime.</p>
+            
+          </div>
+        </div>
+       
+        <div class="content">
+          <h3>SPORTS <span>EVENTS</span></h3>
+          <ul class="movies">
+            <li>
+                <a href="">
+              <h4>Toy Story 3</h4>
+              <img src="images/1page-img2.jpg" alt="" />
+              </a>
+              <p class="text-white">Egetnunc nunc mattitor curabiturpiscipis nec ac hac pellus sem intesque sociis. Metusmetuer hendimentesque diam at orbi sempor.</p>
+              <div class="wrapper"></div>
+             
+            </li>
+            <li>
+                <a href="">
+              <h4>Toy Story 3</h4>
+              <img src="images/1page-img2.jpg" alt="" />
+              </a>
+              <p class="text-white">Egetnunc nunc mattitor curabiturpiscipis nec ac hac pellus sem intesque sociis. Metusmetuer hendimentesque diam at orbi sempor.</p>
+              <div class="wrapper"></div>
+             
+            </li>
+            
+            <li class="clear">&nbsp;</li>
+          </ul>
+        </div>
+      </div>
+      <div id="footer">
+        <div class="left">
+          <div class="right">
+            <div class="footerlink">
+              <p class="lf">Copyright &copy; 2024 <a href="#">SPORTS STREAM| LIVE</a> - All Rights Reserved</p>
+              <p class="rf">Design by <a href="http://www.templatemonster.com/">Sports Stream live Team</a></p>
+              <div style="clear:both;"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script type="text/javascript"> Cufon.now(); </script>
+<!-- END PAGE SOURCE -->
+</body>
+</html>
